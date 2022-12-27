ad3ce0a37aad6ede6aa5d5d326dd155bcac225e6
Sathyaprakash7 <114230325+Sathyaprakash7@users.noreply.github.com>
2022-12-27 13:26:48 +0530

    Add files via upload

---
+++ kycproject.sql
@@ -0,0 +1,6 @@
+create database kycform 
+
+
+create table TB_customerdetails(TbId int primarykey identity(1,1),TbCustomerId varchar(),
+TbCustomername varchar(50), TbGender varchar(50),TbMobile varchar(100),TbDocumentType varchar(50),
+TbIdNumber  varchar(100))
